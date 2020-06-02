0d87e25a0908c5edf1f71866e782786a171b381a
SofyanAudio <55014784+SofyanAudio@users.noreply.github.com>
2020-06-02 08:34:58 +0700

    Add files via upload

---
+++ gambar/index.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Perawat Indo</title>
+  </head>
+  <body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pendaftran</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Jadwal Pemeriksaan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+        </div>
+      </nav>
+
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+          <img src="gambar/MEDIS.jpg" width="25%" class="rounded-circle">
+          <h1 class="display-4">Perawatku</h1>
+          <p class="lead">tenaga medis</p>
+        </div>
+      </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="text-center">About </h1>
+        </div>
+      </div>
+      <div class="row justify-content-center ">
+        <div class="col text-justify">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        </div>
+        <div class="col text-justify">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row pt-5">
+        <div class="col">
+        <h1 class="text-center">Pendaftaran</h1>
+        <div class="col text-left">
+         <form>
+              <div class="form-group">
+                <label for="nama">Nama Anda:</label>
+                <input type="text" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="alamat">Alamat anda:</label>
+                <textarea type="text" class="form-control" id="alamat"></textarea>
+              </div>
+              <div class="form-group">
+                <label for="jk">Jenis Kelamin:</label>
+                <select class="form-control" id="jk">
+                  <option>Pria</option>
+                  <option>Wanita</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="agama">Agama</label>
+                <select class="form-control" id="agama">
+                  <option>Islam</option>
+                  <option>Kristen</option>
+                  <option>Katolik</option>
+                  <option>Buddha</option>
+                  <option>Hindu</option>
+                  <option>Konghucu</option>
+                </select>
+                <div class="form-group">
+                <label for="nama">No. HP</label>
+                <input type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                <label for="nama">No KTP</label>
+                <input type="text" class="form-control">
+                </div>
+                 <div class="form-group">
+                <label for="st">Status Perkawinan:</label>
+                <select class="form-control" id="st">
+                  <option>Kawin</option>
+                  <option>Belum Kawin</option>
+                </select>
+              </div>
+              </div>
+              <button type="submit" class="btn btn-primary">Simpan</button>
+            </form>
+          </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row mb-4 pt-5">
+        <div class="col">
+          <table class="table table-dark">
+            <thead>
+              <tr>
+                <th scope="col">NO</th>
+                <th scope="col">POli</th>
+                <th scope="col">Dokter</th>
+                <th scope="col">Jadwal</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
